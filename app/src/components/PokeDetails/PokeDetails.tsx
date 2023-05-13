f68301d2883b46226ccff453ece171aa438dbf0c
Pablo Domínguez <32307513+pbldmngz@users.noreply.github.com>
2023-05-13 10:47:06 -0700

    feature: add usePokeDetails hook to improve code visibility. Create base concept for the /pokemon/<name> view

--- app/src/components/PokeDetails/PokeDetails.tsx
+++ app/src/components/PokeDetails/PokeDetails.tsx
@@ -1,29 +1,28 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import {
-  RouteParams,
-  PokemonDetailProps,
-} from "Components/PokeDetails/PokeDetailsInterfaces.ts";
+import { RouteParams } from "src/components/PokeDetails/PokeDetailsInterfaces.ts";
+import PokeStats from "src/components/PokeDetails/PokeStats/PokeStats";
+import usePokeDetails from "./usePokeDetails";
 
 type Props = {};
 
 function PokeDetails({}: Props) {
   const { name } = useParams() as RouteParams;
-  const [pokemonDetail, setPokemonDetail] = useState<PokemonDetailProps | null>(
-    null
+  const [pokemonDetail] = usePokeDetails(name);
+
+  if (!pokemonDetail) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <img
+        src={pokemonDetail.sprites.other.dream_world.front_default}
+        alt={name}
+      />
+      <PokeStats stats={pokemonDetail.stats} />
+    </div>
   );
-
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-      .then((response) => response.json())
-      .then((data) => setPokemonDetail(data));
-  }, [name]);
-
-  useEffect(() => {
-    console.log(pokemonDetail);
-  }, [pokemonDetail]);
-
-  return <div>PokeDetails</div>;
 }
 
 export default PokeDetails;
---
+++ app/src/components/PokeDetails/usePokeDetails.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { PokemonDetailProps } from "./PokeDetailsInterfaces";
+
+function usePokeDetails(name: string) {
+  const [pokemonDetail, setPokemonDetail] = useState<PokemonDetailProps | null>(
+    null
+  );
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((response) => response.json())
+      .then((data) => setPokemonDetail(data));
+  }, [name]);
+
+  useEffect(() => {
+    console.log(pokemonDetail);
+  }, [pokemonDetail]);
+
+  return [pokemonDetail];
+}
+
+export default usePokeDetails;
