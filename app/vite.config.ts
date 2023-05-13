55bf45719cb403fe5eec401b5728eaeeee24feba
Pablo Domínguez <32307513+pbldmngz@users.noreply.github.com>
2023-05-13 09:07:23 -0700

    init: configure vite to use absolute paths instead of ../../../../../../

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  esbuild: {
+    loader: "tsx",
+  },
+  root: "./",
+  build: {
+    outDir: "./build",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+        ".ts": "tsx",
+      },
+    },
+  },
+  plugins: [react(), tsconfigPaths()],
+});
