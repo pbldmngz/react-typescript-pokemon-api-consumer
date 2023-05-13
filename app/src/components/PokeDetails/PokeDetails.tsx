8b7b9b09dfd158fa86bd152aefba333d9b99c418
Pablo Domínguez <32307513+pbldmngz@users.noreply.github.com>
2023-05-13 12:28:32 -0700

    feature: add styling to PokeDetails component

--- app/src/components/PokeDetails/PokeDetails.tsx
+++ app/src/components/PokeDetails/PokeDetails.tsx
@@ -3,6 +3,7 @@ import { RouteParams } from "src/components/PokeDetails/PokeDetailsInterfaces.ts
 import PokeStats from "src/components/PokeDetails/PokeStats/PokeStats";
 import usePokeDetails from "./usePokeDetails";
 import "src/components/PokeDetails/PokeDetails.scss";
+import { toTitleCase } from "src/functions/TextOperations";
 
 type Props = {};
 
@@ -17,7 +18,7 @@ function PokeDetails({}: Props) {
   return (
     <div className="view">
       <div className="poke-details-container">
-        <h1>{name}</h1>
+        <h1>{pokemonDetail.name}</h1>
         <div className="poke-details-content">
           <img
             className="poke-image"
@@ -25,8 +26,9 @@ function PokeDetails({}: Props) {
             alt={name}
           />
           <PokeStats stats={pokemonDetail.stats} />
+          <PokeStats stats={pokemonDetail.stats} />
         </div>
-        <footer className="poke-footer">POKEMON API</footer>
+        <footer className="poke-footer">Go back to home</footer>
       </div>
     </div>
   );
