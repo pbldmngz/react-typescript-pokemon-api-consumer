45dcae982fc599e9e1ac3e944056cda155ab67eb
Pablo Domínguez <32307513+pbldmngz@users.noreply.github.com>
2023-05-13 10:45:38 -0700

    fix: fix interface for PokemonDetailProps, add Stats interface

--- app/src/components/PokeDetails/PokeDetailsInterfaces.ts
+++ app/src/components/PokeDetails/PokeDetailsInterfaces.ts
@@ -2,10 +2,25 @@ export interface PokemonDetailProps {
   name: string;
   sprites: {
     front_default: string;
+    other: {
+      dream_world: {
+        front_default: string;
+      };
+    };
   };
+  stats: Stats[];
 }
 
 export interface RouteParams {
   [key: string]: string | undefined;
   name: string;
+}
+
+export interface Stats {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
 }
