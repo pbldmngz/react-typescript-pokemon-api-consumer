896020cc6bab0dc0eb8ca7b971549039b5352adf
Pablo Domínguez <32307513+pbldmngz@users.noreply.github.com>
2023-05-13 14:34:44 -0700

    fix: minor style/organization changes

--- app/src/components/PokeList/PokeList.tsx
+++ app/src/components/PokeList/PokeList.tsx
@@ -1,19 +1,26 @@
-import usePokeList from "./usePokeList";
+import PokeListButtons from "src/components/PokeList/PokeListButtons/PokeListButtons";
+import usePokeList from "src/components/PokeList/usePokeList";
+import PokeListItem from "./PokeListItem/PokeListItem";
 
 function PokeList() {
   const { handleNext, handlePrevious, offset, lastPage, pokemonList } =
     usePokeList();
   console.log("displaying offset", offset, pokemonList[offset]);
 
+  if (!pokemonList[offset]) return <p>Loading...</p>;
+
   return (
-    <div>
-      This is the list
-      <button onClick={handlePrevious} disabled={offset === 0}>
-        Previous
-      </button>
-      <button onClick={handleNext} disabled={lastPage}>
-        Next
-      </button>
+    <div className="view">
+      <div className="pokemon-list-container">
+        <div className="pokemon-list">
+          {pokemonList[offset].map((pokemon) => {
+            return <PokeListItem {...pokemon} key={pokemon.name} />;
+          })}
+        </div>
+        <PokeListButtons
+          {...{ offset, lastPage, handleNext, handlePrevious }}
+        />
+      </div>
     </div>
   );
 }
