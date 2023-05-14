e803ccded4684feb8fa419b7558362a182c1443f
Pablo Domínguez <32307513+pbldmngz@users.noreply.github.com>
2023-05-14 14:15:12 -0700

    feature: redirect all non-existent routes to /list

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import PokeDetails from "src/components/PokeDetails/PokeDetails";
 import PokeList from "src/components/PokeList/PokeList";
 
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<PokeList />} />
+      <Route path="/list" element={<PokeList />} />
       <Route path="/pokemon/:name" element={<PokeDetails />} />
+      <Route path="*" element={<Navigate to="/list" />} />
     </Routes>
   );
 }
