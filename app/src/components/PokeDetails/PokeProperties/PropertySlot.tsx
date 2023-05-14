60f1952711b42be0089e570c0fcc1bb194b5bcd5
Pablo Domínguez <32307513+pbldmngz@users.noreply.github.com>
2023-05-14 08:02:45 -0700

    feature: improve overall styling of PokeDetails

---
+++ app/src/components/PokeDetails/PokeProperties/PropertySlot.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { toTitleCase } from "src/functions/TextOperations";
+import { FlavorTextEntries } from "src/components/PokeDetails/PokeDetailsInterfaces.ts";
+import "src/components/PokeDetails/PokeProperties/PokeProperties.scss";
+
+type Props = { name: string; url: string };
+
+function PropertySlot({ name, url }: Props) {
+  const [propertyDetail, setPropertyDetail] = useState("");
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.flavor_text_entries) {
+          setPropertyDetail(
+            data.flavor_text_entries.find(
+              (entry: FlavorTextEntries) => entry.language.name === "en"
+            ).flavor_text
+          );
+        } else if (data.effect_entries) {
+          setPropertyDetail(
+            data.effect_entries.find(
+              (entry: FlavorTextEntries) => entry.language.name === "en"
+            ).effect
+          );
+        }
+      });
+  }, [name]);
+
+  return (
+    <div className="property-slot">
+      <p className="property-name">{toTitleCase(name)}</p>
+      <p className="property-detail">{propertyDetail}</p>
+    </div>
+  );
+}
+
+export default PropertySlot;
