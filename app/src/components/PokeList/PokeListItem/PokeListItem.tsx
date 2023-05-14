ef3a1e0aa06adbe96d70f68af7ecabb662b576fe
Pablo Domínguez <32307513+pbldmngz@users.noreply.github.com>
2023-05-14 06:26:52 -0700

    feature: use image cache to load images and text for next page before user clicks on next

--- app/src/components/PokeList/PokeListItem/PokeListItem.tsx
+++ app/src/components/PokeList/PokeListItem/PokeListItem.tsx
@@ -1,25 +1,44 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { PokemonListItem } from "src/components/PokeList/PokeListInterfaces";
+import { PokemonContext } from "src/components/PokeList/PokeListContext";
 import "src/components/PokeList/PokeListItem/PokeListItem.scss";
 import { toTitleCase } from "src/functions/TextOperations";
 
 function PokeListItem({ name, url }: PokemonListItem) {
-  const [spriteUrl, setSpriteUrl] = useState("");
+  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+    url.split("/")[6]
+  }.png`;
+
+  const { imageCache, setImageCache } = useContext(PokemonContext);
+  const [loadedImage, setLoadedImage] = useState<HTMLImageElement | null>(null);
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data?.sprites?.front_default) {
-          setSpriteUrl(data.sprites.front_default);
-        }
-      });
-  }, [url]);
+    const cachedImage = imageCache[imageUrl];
+    if (cachedImage) {
+      setLoadedImage(cachedImage);
+    } else {
+      const img = new Image();
+      img.onload = () => {
+        setImageCache((prevCache) => ({ ...prevCache, [imageUrl]: img }));
+        setLoadedImage(img);
+      };
+      img.src = imageUrl;
+    }
+  }, [imageCache]);
+
+  if (!loadedImage) {
+    return (
+      <div className="poke-list-item">
+        <div className="dummy-image"></div>
+        <p>{toTitleCase(name)}</p>
+      </div>
+    );
+  }
 
   return (
     <Link to={`/pokemon/${name}`} className="poke-list-item">
-      {spriteUrl ? <img src={spriteUrl} /> : <div className="dummy-image" />}
+      <img src={loadedImage.src} />
       <p>{toTitleCase(name)}</p>
     </Link>
   );
--- app/src/components/PokeList/usePokeList.ts
+++ app/src/components/PokeList/usePokeList.ts
@@ -1,26 +1,70 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { PokemonContext } from "src/components/PokeList/PokeListContext";
+import { PokemonListItem } from "src/components/PokeList/PokeListInterfaces";
 
 function usePokeList() {
-  const { pokemonList, setPokemonList, offset, setOffset } =
-    useContext(PokemonContext);
-
+  const {
+    pokemonList,
+    setPokemonList,
+    offset,
+    setOffset,
+    imageCache,
+    setImageCache,
+  } = useContext(PokemonContext);
   const [lastPage, setLastPage] = useState(false);
-
+  const isFetching = useRef(false);
   const LIMIT = 21;
 
-  useEffect(() => {
-    if (!pokemonList[offset]) {
-      fetch(
+  const fetchData = async (pageOffset: number, isNextPage = false) => {
+    if (isFetching.current) return;
+    isFetching.current = true;
+
+    try {
+      const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon?offset=${
-          offset * LIMIT
+          pageOffset * LIMIT
         }&limit=${LIMIT}`
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          setLastPage(data.next === null);
-          setPokemonList({ ...pokemonList, [offset]: data.results });
-        });
+      );
+      const data = await response.json();
+      isFetching.current = false;
+      setLastPage(data.next === null);
+      setPokemonList((prevList) => ({
+        ...prevList,
+        [pageOffset]: data.results,
+      }));
+
+      if (isNextPage) {
+        preloadImages(data.results);
+      }
+    } catch (error) {
+      console.error(error);
+      isFetching.current = false;
+    }
+  };
+
+  const preloadImages = (pokemonList: PokemonListItem[]) => {
+    const newImageCache = { ...imageCache };
+
+    pokemonList.forEach((pokemon: PokemonListItem) => {
+      const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+        pokemon.url.split("/")[6]
+      }.png`;
+      const img = new Image();
+      img.onload = () => {
+        newImageCache[url] = img;
+        setImageCache({ ...newImageCache });
+      };
+      img.src = url;
+    });
+  };
+
+  useEffect(() => {
+    if (!pokemonList[offset] || pokemonList[offset].length === 0) {
+      fetchData(offset);
+    }
+
+    if (!pokemonList[offset + 1] || pokemonList[offset + 1].length === 0) {
+      fetchData(offset + 1, true);
     }
   }, [offset]);
 
